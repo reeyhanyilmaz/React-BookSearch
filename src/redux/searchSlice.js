9fe36df479893992b2c726001f1bc5e85a9f6b07
Reyhan Yılmaz <reyhanylmz1993@gmail.com>
2022-09-17 14:42:48 +0300

    create redux folder

---
+++ src/redux/searchSlice.js
@@ -0,0 +1,9 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+export const searchSlice = createSlice({
+    name: "search",
+    initialState: {},
+    reducer: {},
+});
+
+export default searchSlice.reducer;
