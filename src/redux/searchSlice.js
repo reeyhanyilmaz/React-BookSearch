9e309afb99b697868cc4f3440846e3b5a616b030
Reyhan Yılmaz <reyhanylmz1993@gmail.com>
2022-09-20 12:08:42 +0300

    add redux content

--- src/redux/searchSlice.js
+++ src/redux/searchSlice.js
@@ -1,9 +1,43 @@
 import { createSlice } from "@reduxjs/toolkit";
+import { createAsyncThunk } from "@reduxjs/toolkit";
+import axios from "axios";
+
+export const fetchBookData = createAsyncThunk(
+  "search/fetchBookData",
+  async (search) => {
+    // if (search === "") {
+    //   return alert("Arama giriniz");
+    // } else {
+      console.log('search', search);
+      const res = await axios.get(
+        `https://www.googleapis.com/books/v1/volumes?q=intitle:${search}&authors:${search}`
+      );
+      
+      console.log("res", res);
+      return res.data.items;
+    }
+  // }
+);
 
 export const searchSlice = createSlice({
-    name: "search",
-    initialState: {},
-    reducer: {},
+  name: "search",
+  initialState: {
+    search: "", //aramadaki değeri alacak
+    books: [], //arama yapılınca dolacak
+  },
+  reducers: {
+    setSearch: (state, action) => {
+      state.search = action.payload;
+    },
+  },
+  extraReducers: {
+    [fetchBookData.fulfilled]: (state, action) => {
+      state.books = action.payload;
+      console.log("state.books :>> ", state.books);
+      console.log("action.payload", action.payload);
+    },
+  },
 });
 
+export const { setSearch } = searchSlice.actions;
 export default searchSlice.reducer;
